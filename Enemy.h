af6592bb90c5a333bc3ef981b255b22535b72979
Nic14 <nyeungab@connect.ust.hk>
2020-11-22 15:16:25 +0000

    Enemy.h edited Simple stuff (I'm still thinking on other features >_<)

--- Enemy.h
+++ Enemy.h
@@ -1,7 +1,18 @@
 #include "Unit.h"
 
 class Enemy: public Unit {
-    private:
+    protected:
         float shooting_chance;
         int enemy_score;
+    public:
+        //constructor
+        Enemy(float hp, float attack, int velocity[2], int pos[2], int shooting_interval, int MAX_HP, float shooting_chance, int enemy_score);
+
+        //accessor
+        float get_shooting_chance();
+        int get_enemy_score();
+
+        //idk what these are called
+        virtual void shoot();
+        virtual void take_damage(float raw_damage);
 };
